68eb901f543ab9dc7ed6ab806e03b30aaef77f00
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2020-12-06 13:05:37 -0500

    Last Week Update
    All the work I've crammed in the past week

--- public/app/app.js
+++ public/app/app.js
@@ -12,11 +12,11 @@ function initListeners() {
         });
 
         $(".navicon").click(function(e){
-            $('nav').toggleClass("navMobileView");
-            $(".links").toggleClass("linksMobileView");
+            $('#nav').toggleClass("navMobileView");
+            $('#nav nav a').toggleClass("navMobileView");
         })
     
-    }
+}
 
 function initView() {
     $.get('views/nav.html', function(value) {
@@ -33,9 +33,67 @@ function initView() {
     });
 }
 
+var userNumber = 0;
+function init(){
+    // this is for a user signing up
+    $("#submitBtn").click((e) => {
+        e.preventDefault();
+
+        let un = $("#uName").val();
+        let pw = $("#pWord").val();   
+        MODEL.userSignUp(un, pw); 
+
+
+        console.log(`un  ${un} pw ${pw}`);
+    });
+
+    // user logging out
+    $("#logOut").click((e) => {
+        e.preventDefault();
+        MODEL.logOutUser();
+    });
+
+    // user logging in
+    $("#logIn").click((e) => {
+        e.preventDefault();
+        let un = "tnt@t.com";
+        let pw = "password";  
+        MODEL.logInUser(un, pw);
+    });
+
+    // we are adding data
+    $("#addData").click((e) => {
+        e.preventDefault();
+
+        let studentName = "Student" + userNumber;
+        let studentClass = "Class" + userNumber;  
+        userNumber++;
+
+        let stuObject = {
+
+            // Whatever you type here is how it will appear in the database
+            "studentName": studentName,
+            "studentClassNumber": studentClass,
+        };
+
+        MODEL.addStudent(stuObject);
+
+    });
+
+    // this one gets all data
+    $("#getData").click((e) => {
+        e.preventDefault();
+        MODEL.getData();
+    });
+
+}
+
 
 $(document).ready(function() {
+    // Comment out the bottom two functions for access on local host
 
-    initView();
+    MODEL.initFirebase();
+    init();
     
+    initView();
 })
--- public/model/model.js
+++ public/model/model.js
@@ -5,3 +5,93 @@ export function getView(viewScreen) {
     });  
 
 };
+
+var _db;
+
+export function initFirebase() {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            console.log(user);
+            _db = firebase.firestore();
+        } else {
+            console.log("No User");
+            _db = {};
+        }
+    });
+}
+
+export function userSignUp (uName, pWord) {
+    firebase
+    .auth()
+    .createUserWithEmailAndPassword(uName, pWord)
+    .then((result) => {
+        // Alert for successful sign up
+        console.log(result.user.uid);
+    })
+    .catch((error) => {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        // Alert for error message
+        console.log(`Error Code ${errorCode} Error Message ${errorMessage}`)
+    })
+}
+
+export function logInUser(uName, pWord) {
+    firebase
+    .auth()
+    .signInWithEmailAndPassword(uName, pWord)
+    .then((result) => {
+        // Alert for successful sign up
+        console.log(result.user.uid);
+    })
+    .catch((error) => {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        // Alert for error message
+        console.log(`Error Code ${errorCode} Error Message ${errorMessage}`)
+    })
+}
+
+export function logOutUser(){
+    firebase
+    .auth()
+    .signOut()
+    .then(() => {
+        // Use a cool alert for the user saying "Thanks for signing out! Have a nice day!"
+        console.log("User Signed Out")
+    })
+    .catch((error) => {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        // Alert for error
+        console.log(`Error Code ${errorCode} Error Message ${errorMessage}`)
+    })
+}
+
+export function addStudent(studentData) {
+    _db
+        // Whatever you put in the argument for collection, is what the collection will be named in Firebase
+        .collection("Students")
+        .add(studentData)
+        .then((doc) => {
+            console.log(`Student added  ${doc} id ${doc.id}`);
+        })
+        .catch((error) => {
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            // Alert for error
+            console.log(`Error Code ${errorCode} Error Message ${errorMessage}`)
+        });
+}
+
+export function getData() {
+    _db
+    .collection("Students")
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(doc.data())
+            console.log(doc.id);
+        });
+    });
+}
